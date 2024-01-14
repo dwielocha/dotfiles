861ac9937fff2369cc452ea53bfab82d4542ab59
Damian Wielocha <damian@wielocha.com>
2024-01-14 01:20:36 +0100

    waybar calendar icon

--- home.nix
+++ home.nix
@@ -269,7 +269,7 @@
         on-scroll-down = "hyprctl dispatch workspace e-1";
       };
       "clock" = {
-        format = "{:%F :%a : %H:%M}";
+        format = "{: %F, %a  %H:%M}";
         tooltip = true;
         tooltip-format = "<tt><small>{calendar}</small></tt>";
         calendar = {
@@ -278,13 +278,13 @@
           weeks-pos = "right";
           on-scroll = 1;
           on-click-right = "mode";
-          # format = {
-          #   months = "<span color='#ffead3'><b>{}</b></span>";
-          #   days = "<span color='#ecc6d9'><b>{}</b></span>";
-          #   weeks = "<span color='#99ffdd'><b>W{}</b></span>";
-          #   weekdays = "<span color='#ffcc66'><b>{}</b></span>";
-          #   today = "<span color='#ff6699'><b><u>{}</u></b></span>";
-          # };
+          format = {
+            months = "<span color='#ffead3'><b>{}</b></span>";
+            days = "<span color='#ecc6d9'><b>{}</b></span>";
+            weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+            weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+            today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+          };
         };
         actions = {
           on-click-right = "mode";
