f3bf4f0b0a40fa59849712e7c8187495a5ab1374
Damian Wielocha <damian@wielocha.com>
2024-01-14 23:33:12 +0100

    nvm setup in bashrc

--- home.nix
+++ home.nix
@@ -185,6 +185,11 @@
       #  exec Hyprland
       #fi
     '';
+    bashrcExtra = ''
+      export NVM_DIR="$HOME/.nvm"
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+      [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    '';
     sessionVariables = { };
     shellAliases = {
       flake-rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles/#pc";
