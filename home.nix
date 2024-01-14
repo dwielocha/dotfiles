92fb5b7c207beb409a957e6e85bc9753bf3c2f17
Damian Wielocha <damian@wielocha.com>
2024-01-14 23:51:54 +0100

    no bash completion for nvm

--- home.nix
+++ home.nix
@@ -186,7 +186,6 @@
     bashrcExtra = ''
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-      [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
     '';
     sessionVariables = { };
     shellAliases = {
